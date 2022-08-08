9ad536b1671d6600b74e2d6c91ec1580c6dedfce
how200284 <how200284@gmail.com>
2022-08-08 20:50:52 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,6 +7,14 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
   
 })
 
