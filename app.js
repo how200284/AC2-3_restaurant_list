3e17fc735ff722a7ebc21e45386bc9d978280513
how200284 <how200284@gmail.com>
2022-08-09 11:36:49 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 // require internal modules
 const routes = require('./routes')
@@ -16,6 +17,11 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'RestaurantSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(routes)
 
 // listen on and start the Express server
